a881b7ccbb2041cb7bd046ce1df24fcb808a818c
AJITHkm50 <fjt2kor@bosch.com>
2025-09-21 15:11:05 +0530

    Create string_calculator.cpp

---
+++ string_calculator.cpp
@@ -0,0 +1,7 @@
+#include "string_calculator.h"
+
+int StringCalculator::Add(const std::string& numbers) {
+    if (numbers.empty()) return 0;
+    // Further implementation will be added as tests are written
+    return 0;
+}
